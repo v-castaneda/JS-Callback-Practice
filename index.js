7f9ff552b0002e10dcc6da1c194d41428e65f50d
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-06-23 22:59:09 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+const character = newImage("assets/green-character/static.gif");
 
+let direction = null;
+let x = 100;
+let y = 250;
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+function moveCharacter() {
+  if (direction === "west") {
+    x = x - 1;
+  }
+  if (direction === "east") {
+    x = x + 1;
+  }
+  if (direction === "north") {
+    y = y + 1;
+  }
+  if (direction === "south") {
+    y = y - 1;
+  }
+  character.style.left = x + "px";
+  character.style.bottom = y + "px";
+}
+
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
